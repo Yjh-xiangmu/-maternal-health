6153a40f7f87e1dd072b0f0e30bf0dd980735503
Yjh <1828494518@qq.com>
2026-05-11 19:19:02 +0800

    已完成

--- maternal-health/src/main/java/com/example/maternal/MaternalHealthApplication.java
+++ maternal-health/src/main/java/com/example/maternal/MaternalHealthApplication.java
@@ -3,7 +3,7 @@ package com.example.maternal;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//访问 http://localhost:8080/login.html
 @SpringBootApplication
 // 仅保留 basePackages，并确保路径正确指向你的 Mapper 接口包
 @MapperScan(basePackages = "com.example.maternal.mapper")
@@ -12,4 +12,5 @@ public class MaternalHealthApplication {
     public static void main(String[] args) {
         SpringApplication.run(MaternalHealthApplication.class, args);
     }
+
 }
